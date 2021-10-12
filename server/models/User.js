e09bb73a69342b6031d2434ee6e95e4953491efd
Dima <wildface.w@gmail.com>
2021-10-12 16:02:43 +0300

    start server files

---
+++ server/models/User.js
@@ -0,0 +1,16 @@
+const {Schema, model} = require("mongoose")
+
+const User = new Schema ({
+    email: {type: String, required: true, unique: true},
+    password: {type:String, required: true},
+    name: {type: String, required:true},
+    photo:{type: String},
+    status:{type: String},
+    follow:[{type: "Number"}],
+    github:{type: String},
+    facebook:{type: String},
+    linkedin:{type: String},
+    instagram:{type: String},
+})
+
+module.exports = model('User', User)
