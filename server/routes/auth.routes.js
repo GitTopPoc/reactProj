36a060871258e5058caf6d7b12d0d7eb566d96c9
Dima <wildface.w@gmail.com>
2021-10-21 19:18:30 +0300

    server auth/register

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,6 +1,8 @@
 const Router = require("express")
 const User = require("../models/User")
 const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const config = require("config");
 const {check, validationResult} = require("express-validator")
 const router = new Router()
 
@@ -17,13 +19,13 @@ router.post('/registration',
         if (!errors.isEmpty()) {
             return res.status(400).json({message: "Uncorrect request", errors})
         }
-        const {email, name, pasword} = req.body
+        const {email, name, password} = req.body
         const  candidate = await User.findOne({email})
         if (candidate) {
             return res.status(400).json({message: `User with email ${email} already exist`})
         }
-        const  hashPassword = await bcrypt.hash(pasword, 15)
-        const user = new User({email, name, pasword: hashPassword})
+        const  hashPassword = await bcrypt.hash(password, 8)
+        const user = new User({email, name, password: hashPassword})
         await user.save()
         return  res.json({message: "User was created"})
 
@@ -34,5 +36,38 @@ router.post('/registration',
         res.send({message: "Server error"})
     }
 })
+
+router.post('/login', async (req, res) => {
+
+        try {
+           const {email, password} = req.body
+            const user = await User.findOne({email})
+            if (!user) {
+                return res.status(404).json({message: "User not found"})
+            }
+            const isPassValid = bcrypt.compareSync(password, user.password)
+            if (!isPassValid) {
+                return res.status(400).json({message: "Invalid user data"})
+            }
+            const token = jwt.sign({id: user.id}, config.get("secretKey"), {expiresIn: "1h"})
+            return res.json({
+                token,
+                user: {
+                    id: user.id,
+                    email: user.email,
+                    name: user.name,
+                    photo: user.photo,
+                    status: user.status
+                }
+            })
+
+        }
+        catch (e) {
+            console.log(e)
+            res.send({message: "Server error"})
+        }
+    })
+
+
 
 module.exports = router
